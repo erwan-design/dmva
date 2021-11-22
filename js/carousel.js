ab2fb43472e859f170b0ded487fd14fcbfde81dc
Erwan Guillou <erwanguillou@macbook-pro-de-erwan.home>
2021-11-22 10:20:11 +0100

    Edit carousel & mentions

--- js/carousel.js
+++ js/carousel.js
@@ -2,32 +2,21 @@ $('.equipment__carousel').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
     infinite: true,
-    centerMode: true,
-    variableWidth: true,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
           slidesToShow: 2,
-          slidesToScroll: 2
+          slidesToScroll: 1
         }
       },
       {
-        breakpoint: 480,
+        breakpoint: 768,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1
         }
-      }
+      },
       // You can unslick at a given breakpoint now by adding:
       // settings: "unslick"
       // instead of a settings object
