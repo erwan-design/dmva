a690ac9854d92e38c55c8851136d991ba342f824
erwanavem <erwan.guillou@mail.novancia.fr>
2023-06-06 21:41:07 +0200

    Update javascript to display video

--- js/video.js
+++ js/video.js
@@ -1,20 +1,24 @@
 
 /*==================== PLAY VIDEO ====================*/
-  
+
   window.addEventListener('DOMContentLoaded', function() {
-    var video = document.getElementById('myBannerVideo');
-    var fallbackImage = document.getElementById('myFallbackImage');
-  
-    video.addEventListener('loadeddata', function() {
-      fallbackImage.style.display = 'none';
-      video.style.display = 'block';
-      video.play(); // Autoplay the video
-    });
-  
-    video.addEventListener('error', function() {
-      setTimeout(function() {
-        fallbackImage.style.display = 'block';
-        video.style.display = 'none';
-      }, 6000);
-    });
+  var video = document.getElementById('myBannerVideo');
+  var fallbackImage = document.getElementById('myFallbackImage');
+
+  // Event listener for when the video's data is loaded and can be played
+  video.addEventListener('loadeddata', function() {
+    video.style.display = 'block';
+    video.play(); // Autoplay the video
   });
+
+  // Event listener for video loading errors
+  video.addEventListener('error', function() {
+    video.style.display = 'none';
+    fallbackImage.style.display = 'block';
+  });
+
+  // Event listener for when the video playback has started
+  video.addEventListener('play', function() {
+    fallbackImage.style.display = 'none';
+  });
+});
