042ddb389a653f695dc3c3a8419e66ea374712d2
erwanavem <erwan.guillou@mail.novancia.fr>
2023-06-06 18:29:05 +0200

    Update javascript to display video and image if error

--- js/video.js
+++ js/video.js
@@ -1,4 +1,20 @@
-document.addEventListener('DOMContentLoaded', function() {
-var video = document.getElementById('myBannerVideo');
-video.play();
-});
+
+/*==================== PLAY VIDEO ====================*/
+  
+  window.addEventListener('DOMContentLoaded', function() {
+    var video = document.getElementById('myBannerVideo');
+    var fallbackImage = document.getElementById('myFallbackImage');
+  
+    video.addEventListener('loadeddata', function() {
+      fallbackImage.style.display = 'none';
+      video.style.display = 'block';
+      video.play(); // Autoplay the video
+    });
+  
+    video.addEventListener('error', function() {
+      setTimeout(function() {
+        fallbackImage.style.display = 'block';
+        video.style.display = 'none';
+      }, 3000);
+    });
+  });
